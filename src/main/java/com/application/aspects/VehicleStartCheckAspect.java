9d36b5a9d119425e0fc48ac7dace0aac5308bc5a
rashad652 <59527632+rashad652@users.noreply.github.com>
2022-08-01 15:15:01 +0400

    Update VehicleStartCheckAspect.java

--- src/main/java/com/application/aspects/VehicleStartCheckAspect.java
+++ src/main/java/com/application/aspects/VehicleStartCheckAspect.java
@@ -6,14 +6,10 @@ import org.aspectj.lang.annotation.Before;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
-import java.util.logging.Logger;
-
 @Aspect
 @Component
 @Order(1)
 public class VehicleStartCheckAspect {
-
-    private Logger logger = Logger.getLogger(VehicleStartCheckAspect.class.getName());
 
     @Before("execution(* com.application.services.*.*(..)) && args(vehicleStarted,..)")
     public void checkVehicleStarted(JoinPoint joinPoint, boolean vehicleStarted) throws Throwable {
